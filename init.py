d0aaeb90ebf4708446fefa4a639a8e51cd0e3378
AboveColin <colindv2000@gmail.com>
2020-11-05 20:13:08 +0100

    Update init.py

--- init.py
+++ init.py
@@ -5,11 +5,13 @@ import io
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--a')
+parser.add_argument('--a', required=True)
 args = parser.parse_args()
 
 config = ConfigParser.RawConfigParser(allow_no_value=True)
 config.read('config/config.ini')
+
+CDP = config.get('ChromeDriver', 'path')
 
 URL1 = config.get('URL', 'URL1')
 URL2 = config.get('URL', 'URL2')
@@ -22,27 +24,7 @@ E4_1 = config.get('Elements', 'E4_1')
 E4_2 = config.get('Elements', 'E4_2')
 E4_3 = config.get('Elements', 'E4_3')
 
-E5_1 = config.get('Elements', 'E5_1')
-E5_2 = config.get('Elements', 'E5_2')
-E5_3 = config.get('Elements', 'E5_3')
-E5_4 = config.get('Elements', 'E5_4')
-E5_5 = config.get('Elements', 'E5_5')
-E5_6 = config.get('Elements', 'E5_6')
-E5_7 = config.get('Elements', 'E5_7')
-E5_8 = config.get('Elements', 'E5_8')
-
-E6_1 = config.get('Elements', 'E6_1')
-E6_2 = config.get('Elements', 'E6_2')
-E6_3 = config.get('Elements', 'E6_3')
-E6_4 = config.get('Elements', 'E6_4')
-E6_5 = config.get('Elements', 'E6_5')
-E6_6 = config.get('Elements', 'E6_6')
-E6_7 = config.get('Elements', 'E6_7')
-E6_8 = config.get('Elements', 'E6_8')
-E6_9 = config.get('Elements', 'E6_9')
-
-Cn1 = config.get('Cookies', 'Cn1')
-Cn2 = config.get('Cookies', 'Cn2')
+E5 = config.get('Elements', 'E5')
 
 T1 = config.get('Text', 'T1')
 T2 = config.get('Text', 'T2')
