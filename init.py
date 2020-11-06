9b16c8577d0c8d3b35128433fb2bc2ed2cc31f9b
AboveColin <colindv2000@gmail.com>
2020-11-06 10:13:57 +0100

    Update init.py

--- init.py
+++ init.py
@@ -5,7 +5,9 @@ import io
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--a', required=True)
+parser.add_argument('-c', '--composer')
+parser.add_argument('-t', '--title') 
+parser.add_argument('-a', '--author')
 args = parser.parse_args()
 
 config = ConfigParser.RawConfigParser(allow_no_value=True)
@@ -16,7 +18,10 @@ CDP = config.get('ChromeDriver', 'path')
 URL1 = config.get('URL', 'URL1')
 URL2 = config.get('URL', 'URL2')
 
-E1 = config.get('Elements', 'E1')
+E1_1 = config.get('Elements', 'E1_1')
+E1_2 = config.get('Elements', 'E1_2')
+E1_3 = config.get('Elements', 'E1_3')
+
 E2 = config.get('Elements', 'E2')
 E3 = config.get('Elements', 'E3')
 
@@ -29,3 +34,13 @@ E5 = config.get('Elements', 'E5')
 T1 = config.get('Text', 'T1')
 T2 = config.get('Text', 'T2')
 T3 = config.get('Text', 'T3')
+
+if args.composer:
+    mode = E1_3
+    name = args.composer
+elif args.title:
+    mode = E1_1
+    name = args.title
+elif args.author:
+    mode = E1_2
+    name = args.author
