990319746dbd44fdc599c7e0b3586ebd901df400
AboveColin <colindv2000@gmail.com>
2020-11-09 00:45:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ total_pages_rest = T_count % 10
 
 if total_pages_rest != 0:
     last_page = total_pages+1
+else:
+    last_page = total_pages
 
 def next_page(n, max):
     get_T_info(n)
@@ -34,7 +36,7 @@ def next_page(n, max):
 
 def get_T_info(x):
     if x == total_pages + 2:
-        for x in range(1, total_pages_rest + 1):
+        for x in range(1, last_page):
             x = str(x).zfill(2) 
             A_Name = driver.find_element_by_id(config.E4_1 + x + config.E4_2).text
             T_name = driver.find_element_by_id(config.E4_1 + x + config.E4_3).text
@@ -50,9 +52,9 @@ def get_T_info(x):
             print(A_Name + ' - ' + T_name)
 
 
-for x in range(2, total_pages + 3):
+for x in range(2, last_page + 2):
     print()
-    print('=======================')
+    print('========================')
     print('==========' + str(next_page(x, total_pages)) + '/'+str(last_page)+'==========')
 
 driver.quit()
