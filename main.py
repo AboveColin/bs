7c7d3223149385f865e431beca4d32fec800c3fe
AboveColin <colindv2000@gmail.com>
2020-11-09 00:31:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import init as config
+import re
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
@@ -11,35 +12,29 @@ driver = webdriver.Chrome(config.CDP,
 
 driver.delete_all_cookies()
 driver.get(config.URL1)
-search_box = driver.find_element_by_id(config.mode)
-search_box.send_keys(config.name)
-python_button = driver.find_element_by_name(config.E2)
-python_button.click()
+driver.find_element_by_id(config.mode).send_keys(config.name)
+driver.find_element_by_name(config.E2).click()
 
 driver.get(config.URL2)
 
-T_count = int(driver.find_element_by_id(config.E3).text.replace(config.T2, '').replace(config.T3, ''))
-
+T_count = int(re.sub("[^0-9]", "", driver.find_element_by_id(config.E3).text))
 total_pages = T_count // 10
 total_pages_rest = T_count % 10
 
 def next_page(n, max):
     get_T_info(n)
-    P_button = driver.find_element_by_id(config.E5)
-    P_button.click()
+    driver.find_element_by_id(config.E5).click()
     return n-1
-
 
 def get_T_info(x):
     if x == total_pages + 2:
         for x in range(1, total_pages_rest + 1):
-            x = str(x).zfill(2)        
+            x = str(x).zfill(2) 
             A_Name = driver.find_element_by_id(config.E4_1 + x + config.E4_2).text
             T_name = driver.find_element_by_id(config.E4_1 + x + config.E4_3).text
 
             print(A_Name + ' - ' + T_name)
     else:
-
         for x in range(1, 11):
             if x < 10:
                 x = str(x).zfill(2)
