4275c153ac4fc9d86ac56450f7bdc6ebe6d97913
AboveColin <colindv2000@gmail.com>
2020-11-10 00:02:15 +0100

    add the ability to save to file

--- functions.py
+++ functions.py
@@ -11,20 +11,28 @@ def get_T_info(x):
             x = str(x).zfill(2) 
             A_Name = init.driver.find_element_by_id(init.E4_1 + x + init.E4_2).text
             T_name = init.driver.find_element_by_id(init.E4_1 + x + init.E4_3).text
-
-            print(A_Name + ' - ' + T_name)
+            
+            if init.args.save:
+                init.f.write(A_Name + ' - ' + T_name + '\n')
+                print(A_Name + ' - ' + T_name)
+            else:
+                print(A_Name + ' - ' + T_name)
     else:
         for x in range(1, 11):
             if x < 10:
                 x = str(x).zfill(2)
             A_Name = init.driver.find_element_by_id(init.E4_1 + str(x) + init.E4_2).text
             T_name = init.driver.find_element_by_id(init.E4_1 + str(x) + init.E4_3).text
-
-            print(A_Name + ' - ' + T_name)
+            
+            if init.args.save:
+                init.f.write(A_Name + ' - ' + T_name + '\n')
+                print(A_Name + ' - ' + T_name)
+            else:
+                print(A_Name + ' - ' + T_name)
 
 def print_pages():
     for x in range(2, init.last_page + 2):
-        if init.args.list == True:
+        if init.args.list:
             next_page(x, init.total_pages)
         else:
             print()
